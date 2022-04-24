3c57318d0dfb72b51e68da19559fdaef9aa7c573
moni-que <monicahjustus@gmail.com>
2022-04-24 16:43:12 +0300

    create a class

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    '''
+    class that generates new instances of users
+    '''
+
+    # user_credentials = []  #an empty credential list
+
+
+    # def __init__(self,username,password): #creating a function
+    #     self.username = username
+    #     self.password = password
