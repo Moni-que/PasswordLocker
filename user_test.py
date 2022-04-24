ca7e7431e9fc563bffc03ebe9d3d463f07c4ec36
moni-que <monicahjustus@gmail.com>
2022-04-24 16:57:57 +0300

    create a test class and add docstring

--- user_test.py
+++ user_test.py
@@ -1,11 +1,11 @@
 import unittest   #importing the unittest module
 from user import User  #importing the User class
 
-# class TestUser(unittest.TestCase):
+class TestUser(unittest.TestCase):
 
-#     '''
-#     Test class that defines test cases for the user class behaviours
+    '''
+    Test class that defines test cases for the user class behaviours
 
-#     Args:
-#         unittest.TestCase: TestCase class that helps in creating test class
-#     '''
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test class
+    '''
